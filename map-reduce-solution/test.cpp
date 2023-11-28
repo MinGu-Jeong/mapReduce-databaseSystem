cef0d42867056bf80c096ef19ff06c39ddcd4a6c
Jeongminseo <ms0810s@koreatech.ac.kr>
2023-11-28 23:22:04 +0900

    구현완료

--- map-reduce-solution/test.cpp
+++ map-reduce-solution/test.cpp
@@ -16,29 +16,29 @@ struct Pair
     int value;
 };
 
-vector<Pair> mapFunction(const string &text)
+vector<Pair> mapFunction(const string& text)
 {
     vector<Pair> pairs;
     stringstream ss(text);
     string word;
     while (ss >> word)
     {
-        pairs.push_back({word, 1});
+        pairs.push_back({ word, 1 });
     }
     return pairs;
 }
 
-map<string, int> reduce(const vector<Pair> &pairs)
+int reduce(const vector<int>& values)
 {
-    map<string, int> reducedData;
-    for (const auto &pair : pairs)
+    int sum = 0;
+    for (const auto& value : values)
     {
-        reducedData[pair.key] += pair.value;
+        sum += value;
     }
-    return reducedData;
+    return sum;
 }
 
-void processFilePart(const string &filename, int partNumber)
+void processFilePart(const string& filename, int partNumber)
 {
     ifstream file(filename);
     string text((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
@@ -46,13 +46,13 @@ void processFilePart(const string &filename, int partNumber)
 
     // 결과를 로컬 파일에 저장
     ofstream outputFile("localResult" + to_string(partNumber) + ".txt");
-    for (const auto &pair : pairs)
+    for (const auto& pair : pairs)
     {
         outputFile << pair.key << " " << pair.value << '\n';
     }
 }
 
-int splitFile(const string &filename)
+int splitFile(const string& filename)
 {
     ifstream file(filename);
     string line, paragraph;
@@ -73,7 +73,7 @@ int splitFile(const string &filename)
         }
         else
         {
-            paragraph += line + "\n";
+            paragraph += line + "";
         }
     }
 
@@ -99,13 +99,13 @@ int main()
         threads.push_back(thread(processFilePart, "part" + to_string(i) + ".txt", i));
     }
 
-    for (auto &t : threads)
+    for (auto& t : threads)
     {
         t.join();
     }
 
     // 모든 로컬 결과 파일을 병합
-    multimap<string, int> mergedData;
+    map<string, vector<int>> mergedData;
     for (int i = 1; i <= fileCount; ++i)
     {
         ifstream localFile("localResult" + to_string(i) + ".txt");
@@ -113,37 +113,42 @@ int main()
         int value;
         while (localFile >> key >> value)
         {
-            mergedData.insert({ key, value });
+            mergedData[key].push_back(value);
         }
+    }
+
+    // 중간 결과를 파일에 저장
+    ofstream mergeFile("mergeSort.txt");
+    for (const auto& pair : mergedData)
+    {
+        mergeFile << pair.first << ": [";
+        for (int i = 0; i < pair.second.size(); ++i)
+        {
+            mergeFile << pair.second[i];
+            if (i != pair.second.size() - 1)
+            {
+                mergeFile << ",";
+            }
+        }
+        mergeFile << "]" << endl;
     }
 
     // 여기에서 병합된 데이터를 reduce 처리
     map<string, int> finalResult;
-    for (auto it = mergedData.begin(), end = mergedData.end(); it != end; it = mergedData.upper_bound(it->first))
+    for (const auto& it : mergedData)
     {
-        auto range = mergedData.equal_range(it->first);
-        vector<Pair> pairs;
-        for (auto i = range.first; i != range.second; ++i)
-        {
-            pairs.push_back({ i->first, i->second });
-        }
-        map<string, int> reducedData = reduce(pairs);
-        for (const auto& pair : reducedData)
-        {
-            finalResult[pair.first] += pair.second;
-        }
+        finalResult[it.first] = reduce(it.second);
     }
 
     auto end_time = chrono::high_resolution_clock::now();
-    // Sort and Reduce (sort는 map<string, int>에 내장되어 있으므로 필요 없음)
 
     // 최종 결과 출력
-    for (const auto &pair : finalResult)
+    for (const auto& pair : finalResult)
     {
-        cout << pair.first << ": " << pair.second << '\n';
+        cout << pair.first << ": " << pair.second << endl;
     }
 
     auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
-    cout << "Execution time: " << duration << " ms\n";
-    return 0;
+    cout << "Execution time: " << duration << " ms" << endl;
+        return 0;
 }
